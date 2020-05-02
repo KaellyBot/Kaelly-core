79c884e12b58c608ffc4a4c3a2f79e98119bbc3f
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-05-03 00:52:29 +0200

    Assets externalized and better generated

--- src/main/java/com/github/kaysoro/kaellybot/core/mapper/DofusRoomPreviewMapper.java
+++ src/main/java/com/github/kaysoro/kaellybot/core/mapper/DofusRoomPreviewMapper.java
@@ -3,30 +3,26 @@ package com.github.kaysoro.kaellybot.core.mapper;
 import com.github.kaysoro.kaellybot.core.model.constant.Constants;
 import com.github.kaysoro.kaellybot.core.model.constant.Language;
 import com.github.kaysoro.kaellybot.core.payload.dofusroom.PreviewDto;
-import com.github.kaysoro.kaellybot.core.payload.dofusroom.RingDto;
 import com.github.kaysoro.kaellybot.core.payload.dofusroom.StuffDto;
-import com.github.kaysoro.kaellybot.core.payload.dofusroom.TrophusDto;
-import com.github.kaysoro.kaellybot.core.util.ImageProcessor;
+import com.github.kaysoro.kaellybot.core.util.DofusRoomPreviewProcessor;
 import com.github.kaysoro.kaellybot.core.util.Translator;
 import discord4j.core.spec.MessageCreateSpec;
-import org.openimaj.image.ImageUtilities;
+import static com.github.kaysoro.kaellybot.core.util.DofusRoomPreviewProcessor.ItemPosition;
 import org.openimaj.image.MBFImage;
 import org.springframework.stereotype.Component;
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.io.*;
-import java.util.Optional;
 
 @Component
 public class DofusRoomPreviewMapper {
 
     private static final String ATTACHMENT_FILENAME = Constants.NAME + "_" + "Dofusroom_Preview.png";
     private Translator translator;
-    private ImageProcessor imageProcessor;
+    private DofusRoomPreviewProcessor dofusRoomPreviewProcessor;
 
-    public DofusRoomPreviewMapper(Translator translator, ImageProcessor imageProcessor){
+    public DofusRoomPreviewMapper(Translator translator, DofusRoomPreviewProcessor dofusRoomPreviewProcessor){
         this.translator = translator;
-        this.imageProcessor = imageProcessor;
+        this.dofusRoomPreviewProcessor = dofusRoomPreviewProcessor;
     }
 
     public void decorateSpec(MessageCreateSpec spec, PreviewDto preview, Language language){
@@ -42,130 +38,24 @@ public class DofusRoomPreviewMapper {
     }
 
     private InputStream createBuild(PreviewDto preview){
-        try {
-            StuffDto stuff = preview.getData().getItems();
-            MBFImage template = ImageUtilities.readMBF(new File("C:\\Users\\steve\\Desktop\\assets\\dofusroom\\templates\\xelor.png"));
-
-            Optional.ofNullable(stuff.getAmulet()).ifPresent(amulet -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + amulet.getId() + ".png"));
-                    template.drawImage(item, 4, 4);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getRings()).map(RingDto::getTop).ifPresent(ring -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + ring.getId() + ".png"));
-                    template.drawImage(item, 4, 88);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getRings()).map(RingDto::getBottom).ifPresent(ring -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + ring.getId() + ".png"));
-                    template.drawImage(item, 4, 172);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getShield()).ifPresent(shield -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + shield.getId() + ".png"));
-                    template.drawImage(item, 4, 256);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getWeapon()).ifPresent(weapon -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + weapon.getId() + ".png"));
-                    template.drawImage(item, 172, 256);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getCreature()).ifPresent(creature -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + creature.getId() + ".png"));
-                    template.drawImage(item, 256, 256);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getHat()).ifPresent(hat -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + hat.getId() + ".png"));
-                    template.drawImage(item, 424, 4);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getCape()).ifPresent(cape -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + cape.getId() + ".png"));
-                    template.drawImage(item, 424, 88);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getBelt()).ifPresent(belt -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + belt.getId() + ".png"));
-                    template.drawImage(item, 424, 172);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getBoots()).ifPresent(boots -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + boots.getId() + ".png"));
-                    template.drawImage(item, 424, 256);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getTrophus()).map(TrophusDto::getFirst).ifPresent(trophus -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + trophus.getId() + ".png"));
-                    template.drawImage(item, 4, 340);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getTrophus()).map(TrophusDto::getSecond).ifPresent(trophus -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + trophus.getId() + ".png"));
-                    template.drawImage(item, 88, 340);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getTrophus()).map(TrophusDto::getThird).ifPresent(trophus -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + trophus.getId() + ".png"));
-                    template.drawImage(item, 172, 340);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getTrophus()).map(TrophusDto::getFourth).ifPresent(trophus -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + trophus.getId() + ".png"));
-                    template.drawImage(item, 256, 340);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getTrophus()).map(TrophusDto::getFifth).ifPresent(trophus -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + trophus.getId() + ".png"));
-                    template.drawImage(item, 340, 340);
-                } catch(Exception e){}
-            });
-            Optional.ofNullable(stuff.getTrophus()).map(TrophusDto::getSixth).ifPresent(trophus -> {
-                try {
-                    MBFImage item = ImageUtilities.readMBFAlpha(new File(
-                            "C:\\Users\\steve\\Desktop\\assets\\dofusroom\\items_80\\" + trophus.getId() + ".png"));
-                    template.drawImage(item, 424, 340);
-                } catch(Exception e){}
-            });
-
-            ByteArrayOutputStream os = new ByteArrayOutputStream();
-            ImageIO.write(ImageUtilities.createBufferedImageForDisplay(template), "png", os);
-            return new ByteArrayInputStream(os.toByteArray());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return InputStream.nullInputStream();
+        StuffDto stuff = preview.getData().getItems();
+        MBFImage template = dofusRoomPreviewProcessor.getTemplate(preview.getData().getCharacter());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.AMULET, stuff.getAmulet());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TOP_RING, stuff.getRings().getTop());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.BOTTOM_RING, stuff.getRings().getBottom());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.SHIELD, stuff.getShield());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.WEAPON, stuff.getWeapon());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.CREATURE, stuff.getCreature());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.HAT, stuff.getHat());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.CAPE, stuff.getCape());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.BELT, stuff.getBelt());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.BOOTS, stuff.getBoots());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TROPHUS_1, stuff.getTrophus().getFirst());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TROPHUS_2, stuff.getTrophus().getSecond());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TROPHUS_3, stuff.getTrophus().getThird());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TROPHUS_4, stuff.getTrophus().getFourth());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TROPHUS_5, stuff.getTrophus().getFifth());
+        dofusRoomPreviewProcessor.drawItem(template, ItemPosition.TROPHUS_6, stuff.getTrophus().getSixth());
+        return dofusRoomPreviewProcessor.getInputStream(template);
     }
 }
---
+++ src/main/java/com/github/kaysoro/kaellybot/core/util/DofusRoomPreviewProcessor.java
@@ -0,0 +1,92 @@
+package com.github.kaysoro.kaellybot.core.util;
+
+import com.github.kaysoro.kaellybot.core.payload.dofusroom.ItemDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openimaj.image.ImageUtilities;
+import org.openimaj.image.MBFImage;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.imageio.ImageIO;
+import java.io.*;
+import java.util.Optional;
+
+@Component
+public class DofusRoomPreviewProcessor {
+
+    private static final Log LOGGER = LogFactory.getLog(DofusRoomPreviewProcessor.class);
+
+    private static final String TEMPLATE_PATH = "/dofusroom/templates/";
+    private static final String ITEM_PATH = "/dofusroom/items/";
+
+    private String templateDirectory;
+    private String itemDirectory;
+
+    public DofusRoomPreviewProcessor(@Value("${assets.directory}") String baseAssetsDirectory){
+        templateDirectory = baseAssetsDirectory + TEMPLATE_PATH;
+        itemDirectory = baseAssetsDirectory + ITEM_PATH;
+    }
+
+    private MBFImage loadImage(String basePath, String fileName, boolean isTransparent) {
+        try {
+            File file = new File(basePath + fileName + ".png");
+            if (!file.exists()) {
+                LOGGER.warn("Image with the following path does not exist: " + basePath + fileName + ".png");
+                file = new File(basePath + "default.png");
+            }
+            return isTransparent ? ImageUtilities.readMBFAlpha(file) : ImageUtilities.readMBF(file);
+        } catch(IOException e){
+            LOGGER.error(e);
+            return MBFImage.randomImage(1, 1);
+        }
+    }
+
+    public InputStream getInputStream(MBFImage image){
+        try {
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            ImageIO.write(ImageUtilities.createBufferedImageForDisplay(image), "png", os);
+            return new ByteArrayInputStream(os.toByteArray());
+        } catch(IOException e){
+            LOGGER.error(e);
+            return InputStream.nullInputStream();
+        }
+    }
+
+    public MBFImage getTemplate(String character){
+        return loadImage(templateDirectory, character, false);
+    }
+
+    public void drawItem(MBFImage template, ItemPosition itemPosition, ItemDto item){
+        MBFImage imageItem = loadImage(itemDirectory, Optional.ofNullable(item).map(ItemDto::getId)
+                .orElse(itemPosition.getPlaceholder()), true);
+        template.drawImage(imageItem, itemPosition.getX(), itemPosition.getY());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum ItemPosition {
+        AMULET     (4  , 4  , "amulet_placeholder"  ),
+        TOP_RING   (4  , 88 , "ring_placeholder"    ),
+        BOTTOM_RING(4  , 172, "ring_placeholder"    ),
+        SHIELD     (4  , 256, "shield_placeholder"  ),
+        WEAPON     (172, 256, "weapon_placeholder"  ),
+        CREATURE   (256, 256, "creature_placeholder"),
+        HAT        (424, 4  , "hat_placeholder"     ),
+        CAPE       (424, 88 , "cape_placeholder"    ),
+        BELT       (424, 172, "hat_placeholder"     ),
+        BOOTS      (424, 256, "hat_placeholder"     ),
+        TROPHUS_1  (4  , 340, "trophus_placeholder" ),
+        TROPHUS_2  (88 , 340, "trophus_placeholder" ),
+        TROPHUS_3  (172, 340, "trophus_placeholder" ),
+        TROPHUS_4  (256, 340, "trophus_placeholder" ),
+        TROPHUS_5  (340, 340, "trophus_placeholder" ),
+        TROPHUS_6  (424, 340, "trophus_placeholder" );
+
+        private int x;
+        private int y;
+        private String placeholder;
+    }
+}
