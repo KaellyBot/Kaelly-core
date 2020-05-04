aa333d172ca33018e9adb2264dba55c1354bc78d
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-05-04 11:51:32 +0200

    Donate & Invite command removed. Don't know if they will come back again since the content is available in !about.

--- src/main/java/com/github/kaysoro/kaellybot/core/command/argument/model/CommandArgument.java
+++ src/main/java/com/github/kaysoro/kaellybot/core/command/argument/model/CommandArgument.java
@@ -2,10 +2,13 @@ package com.github.kaysoro.kaellybot.core.command.argument.model;
 
 import com.github.kaysoro.kaellybot.core.model.constant.Language;
 import discord4j.core.object.entity.Message;
+import discord4j.rest.util.PermissionSet;
 
 public interface CommandArgument {
 
     boolean triggerMessage(Message message);
+
+    boolean isArgumentHasPermissionsNeeded(PermissionSet permissions);
 
     void execute(Message message);
 
