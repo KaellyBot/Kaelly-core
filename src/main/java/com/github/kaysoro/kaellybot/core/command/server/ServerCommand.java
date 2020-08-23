426afc4f532be51e451b0623765dfefed7aa73ed
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-08-23 18:55:30 +0200

    Serveur command added. Next step => merge the Kaelly-commons lib

---
+++ src/main/java/com/github/kaysoro/kaellybot/core/command/server/ServerCommand.java
@@ -0,0 +1,23 @@
+package com.github.kaysoro.kaellybot.core.command.server;
+
+import com.github.kaysoro.kaellybot.core.command.model.AbstractCommand;
+import com.github.kaysoro.kaellybot.core.command.model.CommandArgument;
+import com.github.kaysoro.kaellybot.core.util.Translator;
+import discord4j.core.object.entity.Message;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@Qualifier(ServerCommand.COMMAND_QUALIFIER)
+public class ServerCommand extends AbstractCommand {
+
+    public static final String COMMAND_QUALIFIER = "ServerCommand";
+    public static final String COMMAND_NAME = "server";
+
+    public ServerCommand(@Qualifier(COMMAND_QUALIFIER) @Lazy List<CommandArgument<Message>> arguments, Translator translator) {
+        super(COMMAND_NAME, arguments, translator);
+    }
+}
